dc82c8e4fc524c9d74bcb78d6e3e3b1423207172
Stiven <stivenmetaj@gmail.com>
2020-07-04 14:19:55 +0200

    maybe we have a good point 4

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import numpy as np
 from matplotlib.pylab import plt
 from network import Graph
-from network import Node
 from tqdm import tqdm
-import copy
+import copy, math
 
 
 def greedy_algorithm(graph, budget, k, verbose=False):
     seeds = []
     spreads = []
     nodes = graph.nodes.copy()
-    print("Budget: " + str(budget) + ", k: " + str(k))
+    #print("Budget: " + str(budget) + ", k: " + str(k))
     for _ in range(budget):
         if verbose:
             print("\n////////////////////////////////////////////////")
@@ -19,7 +18,7 @@ def greedy_algorithm(graph, budget, k, verbose=False):
         best_spread = 0
 
         # For all the nodes which are not seed
-        for node in tqdm(nodes):
+        for node in nodes:
             spread = graph.monte_carlo_sampling(seeds + [node], k, verbose)
 
             if spread > best_spread:
@@ -45,9 +44,10 @@ def greedy_algorithm(graph, budget, k, verbose=False):
             print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n")
     return seeds, spreads[-1]
 
+
 def cumulative_greedy_algorithm(graphs, budget, k, verbose=False):
-    seeds = {g:[] for g in graphs}
-    spreads = {g:0 for g in graphs}
+    seeds = {g: [] for g in graphs}
+    spreads = {g: 0 for g in graphs}
     print("Budget: " + str(budget) + ", k: " + str(k))
     for _ in range(budget):
         if verbose:
@@ -62,7 +62,7 @@ def cumulative_greedy_algorithm(graphs, budget, k, verbose=False):
             for node in tqdm(nodes):
                 spread = graph.monte_carlo_sampling(seeds[graph] + [node], k, verbose)
 
-                spread -= spreads[graph]          # compute marginal increase
+                spread -= spreads[graph]  # compute marginal increase
                 if spread > best_spread:
                     best_spread = spread
                     best_node = node
@@ -81,6 +81,7 @@ def cumulative_greedy_algorithm(graphs, budget, k, verbose=False):
 
     return seeds, spreads
 
+
 def approximation_error(graph, budget, num_experiment):
     plotDict = {}
     k = 2
@@ -88,7 +89,8 @@ def approximation_error(graph, budget, num_experiment):
     for _ in range(0, num_experiment):
         seeds, spread = greedy_algorithm(graph, budget, k, verbose=False)
         print('\nSpread:', spread)  # spread of best configuration executing k montecarlo iterations
-        print('Best seeds:', [s.id for s in seeds], '\n')  # seeds that give the best influence executing k montecarlo iterations
+        print('Best seeds:', [s.id for s in seeds],
+              '\n')  # seeds that give the best influence executing k montecarlo iterations
         plotDict[k] = spread
 
         k = k + int(k * scale_factor)
@@ -99,16 +101,20 @@ def approximation_error(graph, budget, num_experiment):
     plt.plot(x, y)
     plt.show()
 
+
 def cumulative_approximation_error(graphs, budget, num_experiment):
     plotDict = {}
     k = 2
     scale_factor = 0.72
     for _ in range(0, num_experiment):
         seeds, spreads = cumulative_greedy_algorithm(graphs, budget, k, verbose=False)
-        print('\nCumulative spread:', sum(spreads.values()))         # cumulative_spread of best configuration executing k montecarlo iterations
+        print('\nCumulative spread:',
+              sum(spreads.values()))  # cumulative_spread of best configuration executing k montecarlo iterations
         for key, value in seeds.items():
-            print(f'Graph: {key} \tseeds: {[el.id for el in value]}')     # seeds of each graph that give the best influence executing k montecarlo iterations
-        plotDict[k] = sum(spreads.values())     # y axis of the plot shows the cumulative_spread (sum of spread of each single graph)
+            print(
+                f'Graph: {key} \tseeds: {[el.id for el in value]}')  # seeds of each graph that give the best influence executing k montecarlo iterations
+        plotDict[k] = sum(
+            spreads.values())  # y axis of the plot shows the cumulative_spread (sum of spread of each single graph)
 
         k = k + int(k * scale_factor)
 
@@ -118,6 +124,7 @@ def cumulative_approximation_error(graphs, budget, num_experiment):
     plt.plot(x, y)
     plt.show()
 
+
 def point2(graphs, budget, k, num_experiment):
     print('\n--------------------Greedy algorithm---------------------')
 
@@ -126,69 +133,123 @@ def point2(graphs, budget, k, num_experiment):
         print(f'\n------------Graph{g.id}------------')
         seeds, spread = greedy_algorithm(g, budget, k, verbose=False)
         print('\nSpread:', spread)  # spread of best configuration executing k montecarlo iterations
-        print('Best seeds:', [s.id for s in seeds])  # seeds that give the best influence executing k montecarlo iterations
+        print('Best seeds:',
+              [s.id for s in seeds])  # seeds that give the best influence executing k montecarlo iterations
 
     print('\n------------------Approximation Error-------------------')
     # approximation error of the graphs (one at a time)
     for g in graphs:
         approximation_error(g, budget, num_experiment)
 
+
 def point3(graphs, budget, k, num_experiment):
     print('\n---------------Cumulative Greedy algorithm---------------')
 
     # find the best cumulative seeds_set executing k montecarlo iterations
     seeds, spreads = cumulative_greedy_algorithm(graphs, budget, k, verbose=False)
-    print('\nCumulative spread:', sum(spreads.values()))  # cumulative_spread of best configuration executing k montecarlo iterations
+    print('\nCumulative spread:',
+          sum(spreads.values()))  # cumulative_spread of best configuration executing k montecarlo iterations
     print('\nBest seeds:')
     for key, value in seeds.items():
-        print(f'\tGraph: {key.id} \tseeds: {[el.id for el in value]}')  # seeds of each graph that give the best influence executing k montecarlo iterations
+        print(
+            f'\tGraph: {key.id} \tseeds: {[el.id for el in value]}')  # seeds of each graph that give the best influence executing k montecarlo iterations
 
     print('\n-------------Cumulative approximation error--------------')
     # cumulative approximation error of the graphs
     cumulative_approximation_error(graphs, budget, num_experiment)
 
-def point4(graph: Graph, budget, ripetitions, stimulations):
-    # We have to make a copy of the graph before changing the probabilities
-    # In this way we can plot the differences between the original graph and the estimated one
-    true_graph = copy.deepcopy(graph)
 
-    # The initial seeds will be the most "famous" nodes of the graph
-    seeds = [x for x in graph.nodes]
-    seeds.sort(key=lambda x: x.degree)
-    seeds = seeds[:budget]
+def point4(true_graph, budget, repetitions, simulations):
+    # Copy the original graph
+    graph = copy.copy(true_graph)
+    # But set the probabilities to uniform (0.5)
+    graph.adj_matrix = np.where(true_graph.adj_matrix > 0, 0.5, 0)
 
-    # I have to initialize the beta distribution parameters to [1,1]
-    graph.init_estimates()
-    # I have to change the initial probabilities setting them in an uniform way
-    graph.initializeUniformWeights()
+    x_list = []
+    y_list = []
 
-    print(graph.adj_matrix)
-    print()
+    #print(true_graph.adj_matrix)
+    records = []
+
+    # Main procedure
+    for r in tqdm(range(repetitions)):
+        # Make epsilon decrease over time, many explorations at the beginning, many exploitations later
+        epsilon = (1 - r / repetitions) ** 2
+        seeds = choose_seeds(graph, budget, epsilon, simulations)
+        graph.influence_episode(seeds, true_graph.adj_matrix)
+        # test_seeds(graph, seeds)
+        indeces = np.where(graph.adj_matrix > 0)
+
+        # Retrieve for each of them alpha and beta, compute the deviation and update probability
+        for i in range(len(indeces[0])):
+            x = indeces[0][i]
+            y = indeces[1][i]
+            alpha = graph.beta_parameters_matrix[x][y].a
+            beta = graph.beta_parameters_matrix[x][y].b
+            mu = alpha / (alpha + beta)
+            graph.adj_matrix[x][y] = mu
+
+        error = get_total_error(graph, true_graph)
+
+        x_list.append(r)
+        y_list.append(error)
+    print("-------TRUE MATRIX--------")
     print(true_graph.adj_matrix)
-
-    for ripetition in tqdm(range(ripetitions)):
-        for stimulation in range(stimulations):
-            realization_per_node = graph.simulate_episode(seeds)[0]
-            for record in realization_per_node:
-                graph.update_estimations(record[0], record[1])
-        graph.update_weights()
-
-    print()
+    print("-------ESTIMATED MATRIX--------")
     print(graph.adj_matrix)
 
+    return  x_list, y_list
 
 
+def choose_seeds(graph, budget, epsilon, simulations):
+    z = np.random.binomial(1, epsilon)
+    seeds = []
+    if z == 0:
+        # Exploit the available information
+        seeds, _ = greedy_algorithm(graph, budget, simulations)
+    else:
+        # Find the position of the existing edges
+        indeces = np.where(graph.adj_matrix > 0)
 
-graph1 = Graph(10, 0.1)
-graph2 = Graph(250, 0.08)
-graph3 = Graph(350, 0.07)
-graphs = [graph1, graph2, graph3]
+        # Retrieve for each of them alpha and beta, compute the deviation and update probability
+        for i in range(len(indeces[0])):
+            x = indeces[0][i]
+            y = indeces[1][i]
+            alpha = graph.beta_parameters_matrix[x][y].a
+            beta = graph.beta_parameters_matrix[x][y].b
+            mu = alpha / (alpha + beta)
+            sigma = (1 / (alpha + beta)) * np.sqrt((alpha * beta) / (alpha + beta + 1))
+            graph.adj_matrix[x][y] = mu + sigma
 
-budget = 3
-k = 10             # number of montecarlo iterations
-num_experiment = 10
+        # print(graph.adj_matrix)
+        seeds, _ = greedy_algorithm(graph, budget, simulations)
+    return seeds
 
-#point2(graphs, budget, k, num_experiment)
-#point3(graphs, budget, k, num_experiment)
+def get_total_error(graph1: Graph, graph2: Graph):
+    if len(graph1.nodes) == len(graph2.nodes):
+        error = 0
+        total_edges = 0
+        for i in range(len(graph1.nodes)):
+            for j in range(len(graph2.nodes)):
+                if not math.isclose(graph1.adj_matrix[i][j], 0.0):
+                    total_edges += 1
+                    error += abs(graph1.adj_matrix[i][j] - graph2.adj_matrix[i][j])
+        return error/total_edges
 
-point4(graph1, budget, 1000, 1000)
+
+graph1 = Graph(100, 0.15)
+# graph2 = Graph(250, 0.08)
+# graph3 = Graph(350, 0.07)
+# graphs = [graph1, graph2, graph3]
+
+# budget = 3
+# k = 10             # number of montecarlo iterations)
+# num_experiment = 10
+
+# point2(graphs, budget, k, num_experiment)
+# point3(graphs, budget, k, num_experiment)
+
+x, y = point4(graph1, 3, 1000, 10)
+plt.plot(x, y)
+plt.show()
+
--- network.py
+++ network.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-#np.random.seed(12345)
+np.random.seed(12345)
+
 
 class Features:
     def __init__(self, gender, age, location, interests):
@@ -9,6 +10,7 @@ class Features:
         self.location = location
         self.interests = interests
 
+
 class Node(object):
     def __init__(self, id):
         self.id = id
@@ -16,16 +18,16 @@ class Node(object):
         self.neighbors = []
         self.degree = 0
         self.features = self.create_features()
-        self.estimate_parameters = []
+        self.beta_parameters = []
 
     def add_neighborg(self, node):
         self.neighbors.append(node)
         self.degree += 1
 
     def create_features(self):
-        gender = (lambda x: "M" if x < 0.5 else "F")(np.random.uniform(0,1))
+        gender = (lambda x: "M" if x < 0.5 else "F")(np.random.uniform(0, 1))
         age = np.random.randint(5)
-        location = np.array([1]*1 + [0]*5)
+        location = np.array([1] * 1 + [0] * 5)
         np.random.shuffle(location)
         interests = np.random.randint(2, size=6).tolist()
         return Features(gender, age, location, interests)
@@ -34,13 +36,25 @@ class Node(object):
         return f"Node with ID: {self.id}, degree: {self.degree}"
 
 
+class Beta(object):
+    def __init__(self):
+        self.a = 1
+        self.b = 1
+        self.played = 0
+
+    def __str__(self):
+        return f"Beta with alpha:{self.a}, beta:{self.b}, repetitions:{self.played}"
+
+
 class Graph(object):
-    id = 1       # graph id
+    id = 1  # graph id
 
     def __init__(self, n_nodes, connectivity):
         self.n_nodes = n_nodes
         self.connectivity = connectivity
         self.adj_matrix = np.zeros([n_nodes, n_nodes], dtype=np.float)
+        self.beta_parameters_matrix = np.empty((n_nodes, n_nodes), dtype=object)
+        for i in np.ndindex(self.beta_parameters_matrix.shape): self.beta_parameters_matrix[i] = Beta()
         self.nodes = [Node(id) for id in range(self.n_nodes)]
         self.id = Graph.id
         Graph.id += 1
@@ -54,29 +68,18 @@ class Graph(object):
                     self.nodes[i].add_neighborg(self.nodes[j])
                     self.adj_matrix[i][j] = np.random.uniform(0, 0.1)
 
-    def init_estimates(self):
-        for i in self.nodes:
-            for j in range(i.degree):
-                i.estimate_parameters = [[1, 1] for _ in range(i.degree)]
-
-    def initializeUniformWeights(self):
-        for i in range(self.n_nodes):
-            for j in range(self.n_nodes):
-                if self.adj_matrix[i][j] != 0:
-                    self.adj_matrix[i][j] = 1 / len(self.nodes[i].neighbors)
-
-
     # Evaluate influence of n1 over n2
     def evaluate_influence(self, n1, n2):
-        authority = n1.degree/self.n_nodes
-        gender_influence = (lambda x,y: np.random.uniform(0.7,1) if x == y else np.random.uniform(0,0.7)) \
-                           (n1.features.gender,n2.features.gender)
-        age_influence = (lambda x, y: np.random.uniform(0,1)/(abs(x-y)+1)) \
-                        (n1.features.age, n2.features.age)
-        location_influence = (lambda x,y: np.random.uniform(0,1)/(abs(np.where(x == 1)[0][0]-np.where(y == 1)[0][0])+1)) \
-                             (n1.features.location, n2.features.location)
-        interests_influence = np.dot(n1.features.interests, n2.features.interests)/6
-        total_influence = authority*(gender_influence + age_influence + interests_influence)
+        authority = n1.degree / self.n_nodes
+        gender_influence = (lambda x, y: np.random.uniform(0.7, 1) if x == y else np.random.uniform(0, 0.7)) \
+            (n1.features.gender, n2.features.gender)
+        age_influence = (lambda x, y: np.random.uniform(0, 1) / (abs(x - y) + 1)) \
+            (n1.features.age, n2.features.age)
+        location_influence = (
+            lambda x, y: np.random.uniform(0, 1) / (abs(np.where(x == 1)[0][0] - np.where(y == 1)[0][0]) + 1)) \
+            (n1.features.location, n2.features.location)
+        interests_influence = np.dot(n1.features.interests, n2.features.interests) / 6
+        total_influence = authority * (gender_influence + age_influence + interests_influence)
         return total_influence
 
     def monte_carlo_sampling(self, seeds, max_repetition, verbose):
@@ -113,61 +116,37 @@ class Graph(object):
             print(f"Marginal increase of this soltution: {np.mean(nodes_activ_prob)}")
         return np.mean(nodes_activ_prob)
 
-    # In output we have a list where each element has 2 sub-elements:
-    # the first is the id of the activated node (the first k elements of the list will be the initial seeds!),
-    # the second is a list of -1, 0, 1 , having length equal to the degree of the node ->
-    #  0: the neighbor was unactive but the binomial gave us 0
-    #  1: the neighbor was unactive and the binomial gave us 1
-    # -1: the neighbor was already active!
-    # WE WILL USE THIS LIST TO UPDATE THE BETA PARAMETERS FOR EACH EDGE
-    def simulate_episode(self, seeds):
-        realizations_per_node = []
+    def influence_episode(self, seeds, truth):
+        # print("Call to influence episode")
+        for node in self.nodes:
+            if node in seeds:
+                node.activated = True
+            else:
+                node.activated = False
 
-        # I want the ids of the seeds
-        seeds = [seed.id for seed in seeds]
+        binomial_matrix = np.zeros([self.n_nodes, self.n_nodes], dtype=np.float)
+        indeces = np.where(self.adj_matrix > 0)
 
-        for i in seeds:
-            self.nodes[i].activated = True
+        for i in range(len(indeces[0])):
+            x = indeces[0][i]
+            y = indeces[1][i]
+            true_probability = truth[x][y]
+            r = np.random.binomial(1, true_probability)
+            binomial_matrix[x][y] = r
 
-        for i in seeds:
-            realizations = []
-            for j in range(len(self.nodes)):
-                if self.adj_matrix[i][j] != 0:
-                    if not self.nodes[j].activated:
-                        realization = np.random.binomial(1, self.adj_matrix[i][j])
-                        if realization == 1:
-                            seeds.append(j)
-                            self.nodes[j].activated = True
-                        realizations.append(realization)
+        live_edges = binomial_matrix > 0
+
+        new_activated = seeds
+
+        while new_activated:
+            activated = new_activated
+            new_activated = []
+            for active in activated:
+                for neighborg in active.neighbors:
+                    if live_edges[active.id][neighborg.id] and not neighborg.activated:
+                        self.beta_parameters_matrix[active.id][neighborg.id].a += 1
+                        neighborg.activated = True
+                        new_activated.append(neighborg)
                     else:
-                        realizations.append(-1)
+                        self.beta_parameters_matrix[active.id][neighborg.id].b += 1
 
-            realizations_per_node.append([i, realizations])
-
-        nodes_activated = len(seeds)
-
-        for id in seeds:
-            self.nodes[id].activated = False
-
-        return realizations_per_node, nodes_activated
-
-    # Given the id of the node and its realizations of binomial random variables
-    # we update the beta parameters of each edge of that node
-    def update_estimations(self, id, realizations):
-        temp = self.nodes[id].estimate_parameters
-        for i in range(len(realizations)):
-            if realizations[i] != -1:
-                temp[i][0] += realizations[i]
-                temp[i][1] += 1 - realizations[i]
-
-    # We change the probabilities of our adj matrix following the estimated beta parameters!
-    def update_weights(self):
-        for i in self.nodes:
-            id1 = i.id
-            neighbor_id = 0
-            for j in self.nodes:
-                id2 = j.id
-                if self.adj_matrix[id1][id2] != 0:
-                    self.adj_matrix[id1][id2] = np.random.beta(a=i.estimate_parameters[neighbor_id][0],
-                                                               b=i.estimate_parameters[neighbor_id][1])
-                    neighbor_id += 1
